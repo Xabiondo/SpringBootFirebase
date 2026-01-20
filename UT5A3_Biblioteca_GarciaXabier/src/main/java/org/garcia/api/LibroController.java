175366702498e78cb4007f3b19c92054b523de09
xgarciaber <xgarciaber@educacion.navarra.es>
2026-01-20 17:21:13 +0100

    Controller Actualizado con los nuevos services

--- UT5A3_Biblioteca_GarciaXabier/src/main/java/org/garcia/api/LibroController.java
+++ UT5A3_Biblioteca_GarciaXabier/src/main/java/org/garcia/api/LibroController.java
@@ -73,4 +73,104 @@ public class LibroController {
         List<LibroResponse> librosFiltrados = libroService.busquedaPersonalizada(genero, disponible, minimoValoracion, limit);
         return ResponseEntity.ok(librosFiltrados);
     }
+
+    @GetMapping("/autor")
+    public ResponseEntity<List<LibroResponse>> porAutor(@RequestParam String autor) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.porAutor(autor));
+    }
+
+    @GetMapping("/rango-anyo")
+    public ResponseEntity<List<LibroResponse>> porRangoAnyo(@RequestParam int inicio, @RequestParam int fin) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.porRangoAnyo(inicio, fin));
+    }
+
+    @GetMapping("/top")
+    public ResponseEntity<List<LibroResponse>> obtenerTop(@RequestParam int limit) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.obtenerTop(limit));
+    }
+
+    @GetMapping("/cortos-disponibles")
+    public ResponseEntity<List<LibroResponse>> cortosDisponibles(@RequestParam int maxPaginas) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.cortosDisponibles(maxPaginas));
+    }
+
+    @GetMapping("/etiquetas")
+    public ResponseEntity<List<LibroResponse>> porEtiquetas(@RequestParam List<String> etiquetas) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.porEtiquetas(etiquetas));
+    }
+
+    @GetMapping("/titulo-prefijo")
+    public ResponseEntity<List<LibroResponse>> tituloEmpiezaPor(@RequestParam String prefijo) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.tituloEmpiezaPor(prefijo));
+    }
+
+    @GetMapping("/contar-genero")
+    public ResponseEntity<Long> contarGenero(@RequestParam String genero) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.contarGenero(genero));
+    }
+
+    @GetMapping("/paginado")
+    public ResponseEntity<List<LibroResponse>> paginado(@RequestParam int offset, @RequestParam int limit) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.paginado(offset, limit));
+    }
+
+    @GetMapping("/no-disponibles")
+    public ResponseEntity<List<LibroResponse>> obtenerNoDisponibles() throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.obtenerNoDisponibles());
+    }
+
+    @GetMapping("/bestsellers")
+    public ResponseEntity<List<LibroResponse>> bestSellers(@RequestParam int anyoLimite) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.bestSellers(anyoLimite));
+    }
+
+    @GetMapping("/autor-recientes")
+    public ResponseEntity<List<LibroResponse>> autorRecientes(@RequestParam String autor) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.autorRecientes(autor));
+    }
+
+    @GetMapping("/sin-etiquetas")
+    public ResponseEntity<List<LibroResponse>> sinEtiquetas() throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.sinEtiquetas());
+    }
+
+    @GetMapping("/genero-extenso")
+    public ResponseEntity<List<LibroResponse>> generoYExtenso(@RequestParam String genero, @RequestParam int minPaginas) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.generoYExtenso(genero, minPaginas));
+    }
+
+    @GetMapping("/rango-valoracion")
+    public ResponseEntity<List<LibroResponse>> rangoValoracion(@RequestParam double min, @RequestParam double max) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.rangoValoracion(min, max));
+    }
+
+    @GetMapping("/ultima-novedad")
+    public ResponseEntity<List<LibroResponse>> ultimaNovedad() throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.ultimaNovedad());
+    }
+
+    @GetMapping("/generos-exactos")
+    public ResponseEntity<List<LibroResponse>> generosExactos(@RequestParam List<String> generos) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.generosExactos(generos));
+    }
+
+    @GetMapping("/por-anyos")
+    public ResponseEntity<List<LibroResponse>> porAnyos(@RequestParam List<Integer> anyos) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.porAnyos(anyos));
+    }
+
+    @GetMapping("/disponibles-autor")
+    public ResponseEntity<List<LibroResponse>> disponiblesAutor(@RequestParam String autor) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.disponiblesAutor(autor));
+    }
+
+    @GetMapping("/extension-extrema")
+    public ResponseEntity<List<LibroResponse>> porExtensionExtrema(@RequestParam int paginas) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.porExtensionExtrema(paginas));
+    }
+
+    @GetMapping("/autor-valoracion")
+    public ResponseEntity<List<LibroResponse>> autorYValoracion(@RequestParam String autor, @RequestParam double min) throws ExecutionException, InterruptedException {
+        return ResponseEntity.ok(libroService.autorYValoracion(autor, min));
+    }
 }
